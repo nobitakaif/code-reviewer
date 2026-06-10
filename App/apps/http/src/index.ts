a8622834a472615b7692eb890f2fb6f167db629e
mohd kaif <mk2818356@gmail.com>
2026-06-10 23:39:24 +0530

    re-init backend

--- App/apps/http/src/index.ts
+++ App/apps/http/src/index.ts
@@ -1,17 +1,9 @@
-
 import { Elysia } from "elysia";
-import { userAuth } from "./modules/user";
-import { github } from "./modules/github/indes";
+import { userAuth } from "./module/user";
 
-const app = new Elysia({prefix : "/api/v1"})
+const app = new Elysia({ prefix : "/api/v1"})
   .use(userAuth)
-  .get("/test", function(){
-    return {
-      Hello : "world"
-    }
-  })
-  .use(github)
-  .listen(8000);
+  .get("/", () => "Hello Elysia").listen(8000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
---
+++ App/apps/http/src/module/user/index.ts
@@ -0,0 +1,21 @@
+import Elysia, { t } from "elysia";
+
+export const userAuth = new Elysia({prefix : "/auth"})
+    .get("/github", async () =>{
+        const params = new URLSearchParams({
+            client_id : process.env.GITHUB_CLIENT_ID!,
+            scope : "read:user user:email"
+        })
+        return Response.redirect(`https://github.com/login/oauth/authorize?${params}`)
+    })
+    .get("/github/callback", async ({query } ) =>{
+        const { code } = query
+        console.log( query )
+        return {
+            ok : code,
+        }
+    }, {
+        query : t.Object({
+            code : t.String()
+        })
+    })
